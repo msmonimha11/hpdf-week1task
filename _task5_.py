ade16ed01f5831317f5e6696424d6b112a905c14
MAHALAKSHMI S <31372016+msmonimha11@users.noreply.github.com>
2018-01-10 22:52:44 +0530

    remove

--- _task5_.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/robots.txt')
-def index():
-    return render_template('error.html')
-if __name__ == '__main__':
-    app.run(debug=True)
