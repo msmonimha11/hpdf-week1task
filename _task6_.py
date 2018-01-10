b20e997bfbca9bbe72c96b329ec0e9452437e3af
MAHALAKSHMI S <31372016+msmonimha11@users.noreply.github.com>
2018-01-10 22:53:36 +0530

    not needed

--- _task6_.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route('/html')
-def index():
-    return render_template('layout.html')
-if __name__ == '__main__':
-    app.run(debug=True)
